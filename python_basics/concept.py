8005f4e541bb941afd19ae6da9f6d9f26aa879d0
ousmanediallo826 <ousmane.diallo3@mail.citytech.cuny.edu>
2026-04-17 18:44:16 -0400

    Complete dictionaries exercise Day 01

--- python_basics/concept.py
+++ python_basics/concept.py
@@ -13,100 +13,157 @@
 
 # Assignment Expressions
 
-def f(x):
-    return x + 4
-
-
-numbers = [3, 7, 2, 9, 12]
-odd_numbers = [result for x in numbers if(result := f(x)) % 2]
-print(odd_numbers)
+# def f(x):
+#     return x + 4
+#
+#
+# numbers = [3, 7, 2, 9, 12]
+# odd_numbers = [result for x in numbers if(result := f(x)) % 2]
+# print(odd_numbers)
 
 
 #List Manipulation
 
-colours = ["red", "green", "blue", "green", "yellow"]
-colours.remove("green")
-print(colours)
-print(colours.index("green"))
-
-
-lst = ["German is spoken", "in Germany,", "Austria", "Switzerland"]
-lst.insert(3, "and")
-print(lst)
-
-
-#deepcopy
-
-from copy import deepcopy
-
-person1 = ["Swen", ["Seestrasse", "Konstanz"]]
-person2 = deepcopy(person1)
-print(id(person1), id(person2))
-
-
+# colours = ["red", "green", "blue", "green", "yellow"]
+# colours.remove("green")
+# print(colours)
+# print(colours.index("green"))
+#
+#
+# lst = ["German is spoken", "in Germany,", "Austria", "Switzerland"]
+# lst.insert(3, "and")
+# print(lst)
+#
+#
+# #deepcopy
+#
+# from copy import deepcopy
+#
+# person1 = ["Swen", ["Seestrasse", "Konstanz"]]
+# person2 = deepcopy(person1)
+# print(id(person1), id(person2))
+#
+#
 
 # Dictionary in Python
+#
+# person = {
+#     'name': 'Fnu Anchita',
+#     'age': 18,
+#     'city': 'Berlin',
+#     'email': 'anna@example.de'
+# }
+#
+# name = person['name']
+# age = person['age']
+#
+# print("Name:", name)
+# print("Age:", age)
+# person["city"] = "New York"
+# print(person)
+# person['sex'] = 'female'
+# person['favorite_hobbies'] = ['reading', 'music', 'programming']
+# print(person)
+# del person["sex"]
+# print(person)
+#
+#
+#
+#
+# city_population = {
+#     'Berlin': (3769495, 'Germany'),
+#     'Paris': (2161000, 'France'),
+#     'Vienna': (1921641, 'Austria'),
+#     'Hamburg': (1841179, 'Germany'),
+#     'Amsterdam': (872680, 'Netherlands'),
+#     'Rotterdam': (651446, 'Netherlands'),
+#     'Stuttgart': (634830, 'Germany'),
+#     'Zurich': (415215, 'Switzerland'),
+#     'Graz': (290571, 'Austria'),
+#     'Strasbourg': (280966, 'France'),
+#     'Freiburg': (230241, 'Germany'),
+#     'Geneva': (201818, 'Switzerland'),
+#     'Basel': (178128, 'Switzerland'),
+#     'Salzburg': (155031, 'Austria'),
+#     'Luxembourg City': (124528, 'Luxembourg'),
+#     'Metz': (117619, 'France')
+# }
+# city_population['Munich'] = (1471508, 'Germany')
+# city_population['Cologne'] = (1085664, 'Germany')
+# print(city_population)
+#
+#
+# #Convert Dictionaries to Lists
+# city_population_list = list(city_population.items())
+# print(city_population_list)
+# print(city_population_list[0])
+#
+#
+# city_name = input("Please enter your city name: ")
+# population = None
+#
+# for city, (pop, _) in city_population_list:
+#     if city_name.lower() == city.lower():
+#         population = pop
+#         break
+# if population is None:
+#     print(f"The population of {city_name} is {population}")
+# else:
+#     print(f"Population data for {city_name} is not available.")
 
-person = {
-    'name': 'Fnu Anchita',
-    'age': 18,
-    'city': 'Berlin',
-    'email': 'anna@example.de'
-}
-
-name = person['name']
-age = person['age']
-
-print("Name:", name)
-print("Age:", age)
-person["city"] = "New York"
-print(person)
-person['sex'] = 'female'
-person['favorite_hobbies'] = ['reading', 'music', 'programming']
-print(person)
-del person["sex"]
-print(person)
 
 
+city_info_dict = {'Stuttgart': (207.35, 634830), 'Karlsruhe': (173.46, 313092), 'Mannheim': (144.96, 309370), 'Freiburg': (153.07, 230241), 'Heidelberg': (108.83, 160355), 'Heilbronn': (99.88, 125960)}
+city_names = city_info_dict.keys()
+print(city_names)
+
+for city in city_names:
+    print(city)
+
+city_values = city_info_dict.values()
+
+for area, population  in city_values:
+    print("Area:", area, "sq. km, Population:", population)
 
 
-city_population = {
-    'Berlin': (3769495, 'Germany'),
-    'Paris': (2161000, 'France'),
-    'Vienna': (1921641, 'Austria'),
-    'Hamburg': (1841179, 'Germany'),
-    'Amsterdam': (872680, 'Netherlands'),
-    'Rotterdam': (651446, 'Netherlands'),
-    'Stuttgart': (634830, 'Germany'),
-    'Zurich': (415215, 'Switzerland'),
-    'Graz': (290571, 'Austria'),
-    'Strasbourg': (280966, 'France'),
-    'Freiburg': (230241, 'Germany'),
-    'Geneva': (201818, 'Switzerland'),
-    'Basel': (178128, 'Switzerland'),
-    'Salzburg': (155031, 'Austria'),
-    'Luxembourg City': (124528, 'Luxembourg'),
-    'Metz': (117619, 'France')
-}
-city_population['Munich'] = (1471508, 'Germany')
-city_population['Cologne'] = (1085664, 'Germany')
-print(city_population)
+# Turn Lists into Dictionaries
+
+cities = ['Berlin', 'Hamburg', 'Munich', 'Cologne', 'Frankfurt']
+areas = [891.68, 755.3, 310.7, 405.02, 248.31]
+populations = [3769495, 1841179, 1471508, 1085664, 753056]
+
+city_areas_dicts = dict(zip(cities, areas))
+print(city_areas_dicts)
+
+city_info_dicts = {city: (area, population) for city, area, population in zip(cities, areas, populations)}
+print(city_info_dicts)
 
 
-#Convert Dictionaries to Lists
-city_population_list = list(city_population.items())
-print(city_population_list)
-print(city_population_list[0])
+
+#pop() and popitem()
+
+capitals = {"Austria":"Vienna", "Germany":"Berlin", "Netherlands":"Amsterdam", "Switzerland":"Bern"}
+
+capitals_dict = capitals.pop("Switzerland")
+print(capitals_dict)
+print(capitals)
+
+capital = {"Springfield": "Illinois",
+            "Augusta": "Maine",
+            "Boston": "Massachusetts",
+            "Lansing": "Michigan",
+            "Albany": "New York",
+            "Olympia": "Washington",
+            "Toronto": "Ontario"}
+
+(city, state) = capital.popitem()
+print(city, state)
+print(capital)
 
 
-city_name = input("Please enter your city name: ")
-population = None
+# Iterating over a Dictionary
 
-for city, (pop, _) in city_population_list:
-    if city_name.lower() == city.lower():
-        population = pop
-        break
-if population is None:
-    print(f"The population of {city_name} is {population}")
-else:
-    print(f"Population data for {city_name} is not available.")
+d = {"a": 123, "b": 34, "c": 456, "d": 789}
+for k in d:
+    print(k, d[k])
+
---
+++ python_basics/exercises.py
@@ -0,0 +1,112 @@
+# Exercise 1
+# Write a function dict_merge_sum that takes two dictionaries d1 and d2 as parameters.
+# The values of both dictionaries are numerical.
+# The function should return the merged sum dictionary m of those dictionaries.
+# If a key k is both in d1 and d2, the corresponding values will be added and included
+# in the dictionary m If k is only contained in one of the dictionaries,
+# the k and the corresponding value will be included in m
+
+def dict_merge_sum(d1, d2):
+    m = {}
+
+    for k in d1:
+        m[k] = d1[k]
+
+    for k in d2:
+        if k in m:
+            m[k] = m[k] + d2[k]
+        else:
+            m[k] = d2[k]
+    return m
+
+d1 = {'a': 10, 'b': 20, 'c': 30}
+d2 = {'b': 5, 'c': 15, 'd': 40}
+
+result = dict_merge_sum(d1, d2)
+print(result)
+
+
+
+#Exercise 2
+# Given is the following simplified data of a supermarket:
+
+supermarket = { "milk": {"quantity": 20, "price": 1.19},
+               "biscuits":  {"quantity": 32, "price": 1.45},
+               "butter":  {"quantity": 20, "price": 2.29},
+               "cheese":  {"quantity": 15, "price": 1.90},
+               "bread":  {"quantity": 15, "price": 2.59},
+               "cookies":  {"quantity": 20, "price": 4.99},
+               "yogurt": {"quantity": 18, "price": 3.65},
+               "apples":  {"quantity": 35, "price": 3.15},
+               "oranges":  {"quantity": 40, "price": 0.99},
+               "bananas": {"quantity": 23, "price": 1.29}}
+# To be ready for an imminent crisis you decide to buy everything.
+# This isn't particularly social behavior,
+# but for the sake of the task, let's imagine it. The question is how much will you have to pay?
+
+
+def total_price(d):
+    sum = 0
+    for k,v in d.items():
+        sum += v["quantity"] * v["price"]
+    return f"The total price of your grocery is ${sum}"
+
+result = total_price(supermarket)
+print(result)
+
+
+# Exercise 4
+# Given is the island of Vannoth
+
+#Create a dictionary, where we get for every city of Vannoth the distance to the capital city of Rogeburgh
+
+distance_from_capital = {'Rogerburgh': 0,
+                         'Smithstad': 5.2,
+                         'Scottshire': 12.3,
+                         'Clarkhaven': 14.9,
+                         'Dixonshire': 12.7,
+                         'Port Carol': 3.4}
+
+
+
+#Exercise 8
+#Student Gradebook using Nested Dictionaries
+
+#To solve this exercise you have to be familiar with functions in Python, see our introduction on functions in our python course.
+
+#Create a Python program to manage a student gradebook using nested dictionaries. Follow these steps to implement the functionality:
+
+#Initialize an empty dictionary to serve as the gradebook.
+#Implement a function add_student(gradebook, student_name) that takes the gradebook dictionary and a student name as input and adds an empty dictionary for the student's grades to the gradebook.
+#Implement a function add_grade(gradebook, student_name, subject, grade) that takes the gradebook dictionary, a student name, a subject name, and a grade as input. This function should add the grade for the specified subject to the grades dictionary of the specified student in the gradebook.
+#Implement a function calculate_average_grade(gradebook, student_name) that takes the gradebook dictionary and a student name as input and calculates the average grade for the specified student.
+#Implement a function display_gradebook(gradebook) that takes the gradebook dictionary as input and displays the contents of the gradebook, including student names, subjects, and grades, as well as the average grade for each student.
+#Use the provided sample usage to demonstrate the functionality of your program.
+
+#Note: You may assume that each student will have unique names, and subjects may have duplicate names but within the context of different students.
+
+gradeBook = {}
+
+def add_student(gradeBook, student_name):
+    gradeBook[student_name] =  {}
+    return gradeBook
+
+def add_grade(gradeBook, student_name, subject, grade):
+    gradeBook[student_name][subject] = grade
+    return gradeBook
+
+def calculate_average(gradeBook, student_name):
+    total = 0
+    for subject in gradeBook[student_name]:
+        total += gradeBook[student_name][subject]
+    result = total / len(gradeBook[student_name])
+    return result
+def display_gradebook(gradeBook):
+    return gradeBook
+result = add_student(gradeBook, 'Anchita')
+grade1 = add_grade(gradeBook,student_name="Anchita", subject='Math', grade=90)
+grade2 = add_grade(gradeBook,student_name="Anchita", subject='Linear Algebra', grade=30)
+grade3 = add_grade(gradeBook,student_name="Anchita", subject='Science', grade=100)
+grade4 = add_grade(gradeBook,student_name="Anchita", subject='Engineering', grade=70)
+average = calculate_average(gradeBook, 'Anchita')
+print(average)
