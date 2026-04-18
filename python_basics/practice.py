880e5d348372d241530801c695a44b3408b4bc75
ousmanediallo826 <ousmane.diallo3@mail.citytech.cuny.edu>
2026-04-17 21:04:21 -0400

    Practice exercise

---
+++ python_basics/practice.py
@@ -0,0 +1,226 @@
+#LIST EXERCISES (5)
+
+# 1. Filter + Transform
+#
+# Write a function that:
+#
+# Takes a list of integers
+# Returns a new list with only even numbers, each multiplied by 2
+#
+# 👉 Example:
+# [1, 2, 3, 4] → [4, 8]
+
+def new_lists(d):
+    sqrt_even_list = []
+    even_list = []
+    for k in d:
+        if k % 2 == 0:
+            sqrt_even_list.append(k * 2)
+            even_list.append(k)
+    return sqrt_even_list, even_list
+
+
+d = new_lists([1,2,3,4,5,6,7,8,9])
+print(d)
+
+
+
+
+#2. Remove Duplicates (Keep Order)
+
+# Write a function that:
+
+# Takes a list
+# Returns a new list with duplicates removed
+# Must preserve original order
+
+# 👉 Example:
+# [1, 2, 2, 3, 1] → [1, 2, 3]
+
+def remove_duplicates(d):
+    lst = []
+    for i in d:
+        if i not in lst:
+            lst.append(i)
+    return lst
+
+d = remove_duplicates([1,1,2,3,3,4,5,6,6])
+print(d)
+
+
+
+# 3. Find Second Largest
+#
+# Write a function that:
+#
+# Returns the second largest number
+# Do NOT use sort()
+#
+# 👉 Example:
+# [10, 5, 8, 20] → 10
+
+def second_largests(d):
+    largest = float('-inf')
+    second_largest = float('-inf')
+    for k in d:
+        if k > largest:
+            second_largest = largest
+            largest = k
+        elif k > second_largest and k != largest:
+            second_largest = k
+    return second_largest
+
+d = second_largests([10, 5, 8, 20])
+print(d)
+
+
+# 4. Flatten a Nested List
+#
+# Write a function that:
+#
+# Takes a list like [1, [2, 3], [4, [5]]]
+# Returns a fully flattened list
+#
+# 👉 Output:
+# [1, 2, 3, 4, 5]
+
+def flatten_lst(lst):
+    flat_list = []
+    for item in lst:
+        if isinstance(item, list):
+            flat_list.extend(flatten_lst(item))
+        else:
+            flat_list.append(item)
+    return flat_list
+
+lst = flatten_lst( [1, [2, 3], [4, [5]]])
+print(lst)
+
+
+
+
+# 5. Sliding Window Sum
+#
+# Write a function that:
+#
+# Takes a list and integer k
+# Returns list of sums of each window of size k
+#
+# 👉 Example:
+# [1, 2, 3, 4], k=2 → [3, 5, 7]
+
+
+
+
+
+
+# DICTIONARY EXERCISES (5)
+
+# 1. Count Frequency
+#
+# Write a function that:
+#
+# Takes a list
+# Returns a dictionary with frequency of each element
+#
+# 👉 Example:
+
+
+# ['a', 'b', 'a'] → {'a': 2, 'b': 1}
+
+def frequency_element(lst):
+    frequency = {}
+    for item in lst:
+        if item not in frequency:
+            frequency[item] = 1
+        else:
+            frequency[item] += 1
+    return frequency
+lst = frequency_element(['a', 'b', 'a', 'c', 'c', 'c'])
+print(lst)
+
+# 2. Invert Dictionary
+#
+# Write a function that:
+#
+# Swaps keys and values
+# If duplicate values exist → store keys in a list
+#
+# 👉 Example:
+# {'a': 1, 'b': 1} → {1: ['a', 'b']}
+
+def swap_duplicate_value(lst):
+    new_dict = {}
+    for item, value in lst.items():
+        if value not in new_dict:
+            new_dict[value] = [item]
+        else:
+            new_dict[value].append(item)
+    return new_dict
+
+lst=  swap_duplicate_value({'a': 1, 'b': 1, 'c': 2, 'd': 3})
+print(lst)
+
+
+# 3. Group by First Letter
+#
+# Write a function that:
+#
+# Takes a list of words
+# Groups them by first letter
+#
+# 👉 Example:
+# ['apple', 'banana', 'apricot']
+# → {'a': ['apple', 'apricot'], 'b': ['banana']}
+
+def groupping_by_letter(lst):
+    new_dict= {}
+
+    for item in lst:
+        word = item[0]
+        if word not in new_dict:
+            new_dict[word] = [item]
+        else:
+            new_dict[word].append(item)
+    return new_dict
+
+lst = groupping_by_letter(['apple', 'banana', 'apricot', 'coconut', 'car'])
+print(lst)
+
+
+# 4. Nested Sum
+#
+# Write a function that:
+#
+# Takes a dictionary where values can be numbers or dictionaries
+# Returns the total sum of all numbers
+#
+# 👉 Example:
+# {'a': 10, 'b': {'c': 5, 'd': 15}} → 30
+
+def sum_of_numbers(lst):
+    total = 0
+    for value in lst.values():
+        if isinstance(value, dict):
+            total += sum_of_numbers(value)
+        else:
+            total += value
+    return total
+
+lst =  sum_of_numbers({'a': 10, 'b': {'c': 5, 'd': 15}})
+print(lst)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
