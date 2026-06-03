a68beffc18ac7c33d0cd82eea5bf470992daed7e
ousmanediallo826 <ousmane.diallo3@mail.citytech.cuny.edu>
2026-06-03 15:50:19 -0400

    4. Threads and Threading

--- python_applications/concept.py
+++ python_applications/concept.py
@@ -138,3 +138,46 @@ else:
     os.wait()
     print(f"Parent Inventory State: {shared_inventory}")
 
+
+
+
+#========================4. Threads and Threading==========================
+import threading
+def check_printer_status(printer_id: str):
+    print(f"[Thread-{printer_id}]: Checking toner and printer level")
+    time.sleep(2)
+    print(f"[Thread-{printer_id}] Printer check complete.")
+
+thread1 = threading.Thread(target=check_printer_status, args=("PRN-01",))
+thread2 = threading.Thread(target=check_printer_status, args=("PRN-02",))
+
+thread1.start()
+thread2.start()
+
+thread01 = thread1.join()
+thread02 = thread2.join()
+
+
+print("Main Script: Both printer threads finished. Updating library dashboard.")
+
+
+total_student_count = 0
+
+counter_lock = threading.Lock()
+
+def student_gate_scanner():
+    global total_student_count
+    for _ in range(100000):
+        with counter_lock:
+            total_student_count += 1
+gate1 = threading.Thread(target=student_gate_scanner)
+gate2 = threading.Thread(target=student_gate_scanner)
+
+gate1.start()
+gate2.start()
+
+gate1.join()
+gate2.join()
+
+print(f"Final safe student count: {total_student_count}")
+
